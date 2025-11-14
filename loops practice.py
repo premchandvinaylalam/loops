4b86a644dbb7dffff8c7584f194a7330b58c792e
PremChand <lpremchandvinay@gmail.com>
2025-11-14 11:14:19 +0530

    lo

--- loops practice.py
+++ loops practice.py
@@ -87,3 +87,11 @@ for i in range(1,n+1):
   ***
  ****
 *****'''
+
+n = 4
+'''for i in range(1,n+1):
+    for j in range(1,n+3):
+        print(" "*(n-j)+"*"*i+" "*(n-j))'''
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
