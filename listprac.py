bce87a4f42985240f859023ebcb80b165aa38467
PremChand <lpremchandvinay@gmail.com>
2025-11-17 23:36:04 +0530

    odd

---
+++ listprac.py
@@ -0,0 +1,43 @@
+a = [23,27,1,15,34]
+for i in a:
+    if i%5!=0:
+        continue
+    elif i%5==0:
+        print(i)
+        break
+print("stop")
+
+
+for i in range(1,11):
+    n = int(input("enter the number"))
+    if n<0:
+        print("stopped at negative number")
+        break
+    else:
+        print(n)
+print("END")
+
+
+'''n = input("enter the word")
+for i in n:
+    if list(n)=="a" or "e" or "i" or "o" or "u":
+        break
+    else:
+        print(n)'''
+
+n = input("enter the word: ")
+
+for i in n:
+    if i in ("a", "e", "i", "o", "u"):
+        break
+    else:
+        print(i)
+
+
+for i in range(1,21):
+    if i==13:
+        break
+    elif i%2==0:
+        continue
+    elif i%2!=0:
+        print(i)
